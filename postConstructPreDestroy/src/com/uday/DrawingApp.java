2d16bde303465fc76bfeae7da8e2b11cff392470
ubanda <ubanda@LIN66013053.corp.capgemini.com>
2021-09-19 18:56:19 +0530

    recommit

--- postConstructPreDestroy/src/com/uday/DrawingApp.java
+++ postConstructPreDestroy/src/com/uday/DrawingApp.java
@@ -1,5 +1,19 @@
 package com.uday;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class DrawingApp {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring-config.xml"));
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+		context.registerShutdownHook();
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+
+
+	}
 
 }
--- postConstructPreDestroy/src/com/uday/Triangle.java
+++ postConstructPreDestroy/src/com/uday/Triangle.java
@@ -1,5 +1,28 @@
 package com.uday;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class Triangle {
 
+	public void draw() {
+		System.out.println("drawn triangle");
+	}
+	
+//	@Required
+//	
+//	public void initializeTriangle() {
+//		System.out.println("init triangle");
+//	}
+//	
+	@PostConstruct
+	public void initializeTriangle() {
+		System.out.println("init triangle");
+	}
+	
+	@PreDestroy
+	public void destroyTriangle() {
+		System.out.println("Destruction level 3000");
+	}
+	
 }
